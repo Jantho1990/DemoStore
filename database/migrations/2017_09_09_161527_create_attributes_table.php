81839e8a6f334e61a6f7afe5331d69cd28b42c88
Josh Anthony <janthony@brokerbin.com>
2017-09-12 20:26:02 -0500

    added migrations

--- database/migrations/2017_09_09_161527_create_attributes_table.php
+++ database/migrations/2017_09_09_161527_create_attributes_table.php
@@ -15,6 +15,26 @@ class CreateAttributesTable extends Migration
     {
         Schema::create('attributes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('type', 16);
+            $table->integer('integer_value');
+            $table->unsignedInteger('unsigned_integer_value')->unsigned();
+            $table->string('string_value');
+            $table->text('text_value');
+            $table->longtext('long_text_value');
+            $table->boolean('boolean_value');
+            $table->date('date_value');
+            $table->time('time_value');
+            $table->dateTime('date_time_value');
+            $table->decimal('decimal_value');
+            $table->float('float_value');
+            $table->double('double_value');
+            $table->json('json_value');
+
+            // These are fake value types that we will need to work some magic to make them configurable.
+            $table->json('enum_value');
+            $table->integer('increment_value');
+
             $table->timestamps();
         });
     }
